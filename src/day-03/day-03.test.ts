04f266a142eb812685c22d3198976bd63349c966
spiney <mindthespines@pm.me>
2022-12-18 10:49:15 -0800

    solve day 3 part 2

--- src/day-03/day-03.test.ts
+++ src/day-03/day-03.test.ts
@@ -27,14 +27,19 @@ describe("day 3 tests", () => {
     expect(actual).toEqual(expected);
   });
 
-  it("gets the sum of the priorities of the duplicated items in each rucksack from the input", async () => {
+  it("gets the sum of the priorities of the duplicated items and the sum of the priorities of each group's badges in each rucksack from the input", async () => {
     const rucksackContents = await setUpRucksacks();
     const rucksacks = rucksackContents.map(
       (contents) => new Rucksack(contents)
     );
     const collection = new RucksackCollection(rucksacks);
-    const actual = collection.dupePrioritySum;
-    const expected = 8053;
-    expect(actual).toEqual(expected);
+    
+    const actualDupe = collection.dupePrioritySum;
+    const expectedDupe = 8053;
+    expect(actualDupe).toEqual(expectedDupe);
+
+    const actualBadge = collection.badgePrioritySum;
+    const expectedBadge = 2425;
+    expect(actualBadge).toEqual(expectedBadge);
   });
 });
